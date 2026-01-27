25474022aa9a6801ae6b59c2f44ef845bb6e6bcb
LinkIsGrim <69561145+LinkIsGrim@users.noreply.github.com>
2026-01-27 20:06:18 -0300

    feat:hide ACE Zeus AI modules

---
+++ addons/zeus/CfgVehicles.hpp
@@ -0,0 +1,18 @@
+class CfgVehicles {
+    class ACEGVAR(zeus,moduleBase);
+    class ACEGVAR(zeus,moduleDefendArea): ACEGVAR(zeus,moduleBase) {
+        scope = 0;
+    };
+    class ACEGVAR(zeus,moduleGarrison): ACEGVAR(zeus,moduleBase) {
+        scope = 0;
+    };
+    class ACEGVAR(zeus,moduleUnGarrison): ACEGVAR(zeus,moduleBase) {
+        scope = 0;
+    };
+    class ACEGVAR(zeus,moduleSearchArea): ACEGVAR(zeus,moduleBase) {
+        scope = 0;
+    };
+    class ACEGVAR(zeus,moduleSearchNearby): ACEGVAR(zeus,moduleBase) {
+        scope = 0;
+    };
+};
