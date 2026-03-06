412f12676dd736e96559a9480439316a090cc583
Grim <69561145+LinkIsGrim@users.noreply.github.com>
2026-03-06 19:29:36 -0300

    feat: weightless helmet camera (#26)
    and fix the base arsenal

---
+++ addons/equipment_tweaks/ctab/CfgWeapons.hpp
@@ -0,0 +1,10 @@
+class CfgWeapons {
+    class CBA_MiscItem;
+    class CBA_MiscItem_ItemInfo;
+
+    class ItemcTabHCam: CBA_MiscItem {
+        class ItemInfo: CBA_MiscItem_ItemInfo {
+            mass = 0; // 0 mass, assume it's part of the helmet or something
+        };
+    };
+};
