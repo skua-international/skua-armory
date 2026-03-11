1c5de628798b62d338c044c93bc97ff4176a4ec5
LinkIsGrim <69561145+LinkIsGrim@users.noreply.github.com>
2026-03-11 19:05:16 -0300

    fix: 60mm mortar CSW properties

---
+++ addons/equipment_tweaks/m224/CfgVehicles.hpp
@@ -0,0 +1,35 @@
+class CfgVehicles {
+    class Bag_Base;
+    // Fix the backpack to spawn the correct mortar variant
+    class NDS_B_M224_mortar: Bag_Base {
+        class assembleInfo {
+            assembleTo = "avm224_M224_mortar";
+        };
+    };
+    
+    class StaticMortar;
+    class Mortar_01_base_F : StaticMortar {
+        class ace_csw;
+    };
+    // Enable fire on load for the 60mm CSW variant
+    class NDS_M224_mortar_base: Mortar_01_base_F {
+        class ace_csw: ace_csw {
+            enableFireOnLoad = 1;
+            ammoLoadTime = 0.5;
+            ammoUnloadTime = 0.5;
+            proxyWeapon = "avm224_W_M224_mortar_proxy";
+            disassembleTo = "avm224_W_M224_mortar_carry";
+        };
+    };
+
+    class NDS_M224_mortar;
+    class avm224_M224_mortar: NDS_M224_mortar {
+        class ace_csw {
+            enableFireOnLoad = 1;
+            ammoLoadTime = 0.5;
+            ammoUnloadTime = 0.5;
+            // proxyWeapon = "avm224_W_M224_mortar_proxy";
+            // disassembleTo = "avm224_W_M224_mortar_carry";
+        };
+    };
+};
