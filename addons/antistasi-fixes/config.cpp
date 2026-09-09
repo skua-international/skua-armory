8bfc1d62089a81113e2bce98beb1088bfc8a419e
Grim <69561145+LinkIsGrim@users.noreply.github.com>
2026-09-09 04:51:18 -0300

    feat(antistasi): antistasi-fixes addon (#55)
    - Loot crates and reward boxes of all kinds carryable with no weight/ace
    cargo loadable with size=1, space=0 + considered an antistasi loot item;
    - "Pack to box" interaction makes a real loot crate;
    - Medical tents spawned from garage persist on mission restart;
    - Medical tents have scroll wheel action to spawn a medical box (same
    costs etc. as buying)
    - Loot crates and reward boxes of all kinds directly garageable;
    - Adds "Combat Outposts" which are just a normal outpost that will
    actively engage enemies, colored red on the map;
    
    _not_ making gather loot handle outpost boxes etc. because that can be
    exploited.

---
+++ addons/antistasi-fixes/config.cpp
@@ -0,0 +1,21 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "skua_main",
+            "A3A_core", // Present on CE/Ultimate/TEH alike - just used to gate loading to an Antistasi mission at all. The loot-crate-classname lookups below are all runtime/dynamic (A3A_faction_reb get "lootCrate") specifically so this addon doesn't need to hardcode a classname that differs - or is entirely absent, on CE - per variant.
+            "ace_dragging",
+            "ace_cargo"
+        };
+        skipWhenMissingDependencies = 1;
+        author = "LinkIsGrim";
+        name = COMPONENT_NAME;
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgEventHandlers.hpp"
